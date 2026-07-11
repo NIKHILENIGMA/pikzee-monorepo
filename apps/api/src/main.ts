55e8fce397f72a78a472194f15141df844ad2a97
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-07-11 12:20:45 +0000

    fix(api): update port environment variable to use PORT instead of API_PORT

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
   app.useGlobalPipes(new ZodValidationPipe()) // Add ZodValidationPipe globally to validate incoming requests using Zod schemas
-  const port = process.env.API_PORT || 3000
+  const port = process.env.PORT || 3000
   await app.listen(port)
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`)
 }
