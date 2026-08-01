c2f9e97e46b903030a886f5b93c865311fafbcb7
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-08-01 02:41:36 -0700

    chore(api): configure jest testing framework
        - Added Jest configuration files for the root workspace and api app
        - Updated tsconfig and project.json to support test execution
        - Aligned jest dependencies to version 29.x in package.json
        - Added vscode-jest-runner extension recommendation

---
+++ apps/api/jest.config.cjs
@@ -0,0 +1,18 @@
+ 
+module.exports = {
+  displayName: 'api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/api',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/api-e2e/'],
+}
